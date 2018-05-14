bfa07cf16c68de6a1d16f7f2ef94b11cf7934b96
zht <450784263@qq.com>
2018-05-14 23:06:04 +0800

    更新垃圾富民

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '垃圾富民',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
