f57f75e650cfe89d53c56e4614a9d0c23956aa50
zht <450784263@qq.com>
2018-05-14 23:15:23 +0800

    辣鸡涛哥

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '垃圾涛哥',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
