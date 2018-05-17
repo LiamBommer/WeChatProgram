fc31213c94d8fd0906142d16c2f411844702d885
Dawnman <523362613@qq.com>
2018-05-17 09:59:43 +0800

    fetch

---
+++ pages/Meeting/meetingDetail/meetingDetail.js
@@ -0,0 +1,13 @@
+// meetingDetail.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    title: 'Meeting Today',
+    content: "We don't have to do anythin today! "
+  },
+  onLoad: function () {
+
+  }
+})
--- pages/Project/Meeting/Meeting.js
+++ pages/Project/Meeting/Meeting.js
@@ -8,6 +8,12 @@ Page({
   
   },
 
+  build: function(){
+   wx.navigateTo({
+     url: './addMeeting/addMeeting',
+   })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
