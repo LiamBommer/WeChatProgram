f3dd7e1a767db7319874802c293610f52789e77f
Dawnman <523362613@qq.com>
2018-06-14 00:38:10 +0800

    修改了获取用户授权的逻辑。

--- app.js
+++ app.js
@@ -24,77 +24,84 @@ App({
             that.globalData.userId = user.id
             that.globalData.nickName = user.get("nickName")
             that.globalData.userPic = user.get("userPic")
-
-          } else {
-
-            //注册成功的情况
-            var u = Bmob.Object.extend("_User");
-            var query = new Bmob.Query(u);
-            query.get(user.id, {
-              success: function (result) {
-                wx.setStorageSync('own', result.get("uid"));
-                //将userId，存储到缓存中
-                wx.setStorageSync('userId', user.id)
-              },
-              error: function (result, error) {
-                console.log("查询失败");
-              }
-            });
-
-
-            //保存用户其他信息，比如昵称头像之类的
-            wx.getSetting({
-              success: res => {
-                if (res.authSetting['scope.userInfo']) {
-                  // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                  wx.getUserInfo({
-                    success: res => {
-                      var userInfo = res.userInfo;
-                      var nickName = userInfo.nickName;
-                      var avatarUrl = userInfo.avatarUrl;
-                      var gender = userInfo.gender;
-                      that.globalData.userId = user.id
-                      that.globalData.nickName = user.get("nickName")
-                      that.globalData.userPic = user.get("userPic")
-                      console.log(that.globalData.userId)
-
-                      var u = Bmob.Object.extend("_User");
-                      var query = new Bmob.Query(u);
-                      // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
-                      query.get(user.id, {
-                        success: function (result) {
-                          // 自动绑定之前的账号
-                          result.set("nickName", nickName);
-                          result.set("userPic", avatarUrl);
-                          result.set("openid", openid);
-                          //result.set("gender",gender);  //再添加数据就不能正常初始化了
-                          result.save();
-
-                        }
-                      })
-                      //为用户添加空的项目“我的项目”
-                      that.buildProject('我的项目','空项目')
-                      //为用户添加实例沟通模板
-                      that.addCommunicateModel(user.id,1,communicate_sample_model1)
-                      
-                      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                      // 所以此处加入 callback 以防止这种情况
-                      if (that.userInfoReadyCallback) {
-                        that.userInfoReadyCallback(res)
-                      }
-                    }
-                  })
-                } else {
-                  // 没有授权，弹出授权页面
-                  that.globalData.userId = user.id
-                  wx.navigateTo({
-                    url: '../GetUserInfo/GetUserInfo',
-                  })
-
-                }
-              }
+            wx.switchTab({
+              url: '../Project/Project',
             })
-          }
+          } 
+          // else {
+
+          //   //注册成功的情况
+          //   var u = Bmob.Object.extend("_User");
+          //   var query = new Bmob.Query(u);
+          //   query.get(user.id, {
+          //     success: function (result) {
+          //       wx.setStorageSync('own', result.get("uid"));
+          //       //将userId，存储到缓存中
+          //       wx.setStorageSync('userId', user.id)
+          //     },
+          //     error: function (result, error) {
+          //       console.log("查询失败");
+          //     }
+          //   });
+
+
+          //   //保存用户其他信息，比如昵称头像之类的
+          //   wx.getSetting({
+          //     success: res => {
+          //       if (res.authSetting['scope.userInfo']) {
+          //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          //         wx.getUserInfo({
+          //           success: res => {
+          //             var userInfo = res.userInfo;
+          //             var nickName = userInfo.nickName;
+          //             var avatarUrl = userInfo.avatarUrl;
+          //             var gender = userInfo.gender;
+          //             that.globalData.userId = user.id
+          //             that.globalData.nickName = user.get("nickName")
+          //             that.globalData.userPic = user.get("userPic")
+          //             console.log(that.globalData.userId)
+
+          //             var u = Bmob.Object.extend("_User");
+          //             var query = new Bmob.Query(u);
+          //             // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
+          //             query.get(user.id, {
+          //               success: function (result) {
+          //                 // 自动绑定之前的账号
+          //                 result.set("nickName", nickName);
+          //                 result.set("userPic", avatarUrl);
+          //                 result.set("openid", openid);
+          //                 //result.set("gender",gender);  //再添加数据就不能正常初始化了
+          //                 result.save();
+
+          //               }
+          //             })
+          //             //为用户添加空的项目“我的项目”
+          //             //that.buildProject('我的项目','空项目')
+          //             //为用户添加实例沟通模板
+          //             //that.addCommunicateModel(user.id,1,communicate_sample_model1)
+                      
+          //             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          //             // 所以此处加入 callback 以防止这种情况
+          //             if (that.userInfoReadyCallback) {
+          //               that.userInfoReadyCallback(res)
+          //             }
+          //           }
+          //         })
+          //       }
+                
+          //     }
+              
+          //   })
+          // }
+          // else {
+          //   // 没有授权，弹出授权页面
+          //   that.globalData.userId = user.id
+          //   wx.redirectTo({
+          //     url: '../GetUserInfo/GetUserInfo',
+          //   })
+
+          // }
+          
 
         }, function (err) {
           console.log(err, 'errr');
@@ -229,6 +236,7 @@ App({
   addLeader: function (projId, userId) {
     var ProjectMember = Bmob.Object.extend("proj_member")
     var projMember = new ProjectMember();
+    var project = Bmob.Object.createWithoutData('project',projId)
 
     projMember.save({
       proj_id: projId,
--- pages/GetUserInfo/GetUserInfo.js
+++ pages/GetUserInfo/GetUserInfo.js
@@ -1,6 +1,6 @@
 
 const Bmob = require('../../utils/bmob.js')
-
+var user = new Bmob.User();//实例化
 Page({
 
   /**
@@ -13,52 +13,202 @@ Page({
   /**
    * 点击按钮，确认获取信息
    */
-  confirm: function () {
+  confirm: function (res) {
+    console.log('confirm',res)
+    wx.login({
+       success: function (res) {
+         user.loginWithWeapp(res.code).then(function (user) {
+            var openid = user.get("authData").weapp.openid;
+            // 查看是否授权
+            wx.getSetting({
+              success: function (res) {
+                if (res.authSetting['scope.userInfo']) {
+                  // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+                  wx.getUserInfo({
+                    success: function (res) {
+                      console.log('获取用户信息成功', res.userInfo)
+                      getApp().globalData.userId = user.id
+                      getApp().globalData.nickName = res.userInfo.nickName
+                      getApp().globalData.userPic = res.userInfo.avatarUrl
 
-    // 查看是否授权
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserInfo({
-            success: function (res) {
-              console.log('获取用户信息成功', res.userInfo)
+                      // 存进数据库
+                      var u = Bmob.Object.extend("_User");
+                      var query = new Bmob.Query(u);
 
-              var userId = getApp().globalData.userId
-              var nickName = res.userInfo.nickName
-              var avatarUrl = res.userInfo.avatarUrl
-              var openid = getApp().globalData.openid
-              getApp().globalData.nickName = res.userInfo.nickName
-              getApp().globalData.userPic = res.userInfo.avatarUrl
-
-              // 存进数据库
-              var u = Bmob.Object.extend("_User");
-              var query = new Bmob.Query(u);
-              // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
-              query.get(userId, {
-                success: function (result) {
-                  // 自动绑定之前的账号
-                  result.set("nickName", nickName);
-                  result.set("userPic", avatarUrl);
-                  result.set("openid", openid);
-                  //result.set("gender",gender);  //再添加数据就不能正常初始化了
-                  result.save();
-
+                      var userId = getApp().globalData.userId
+                      var nickName = res.userInfo.nickName
+                      var avatarUrl = res.userInfo.avatarUrl
+                      // 这个 id 是要修改条目的 id，你在生成这个存储并成功时可以获取到，请看前面的文档
+                      query.get(userId, {
+                        success: function (result) {
+                          // 自动绑定之前的账号
+                          result.set("nickName", nickName);
+                          result.set("userPic", avatarUrl);
+                          result.set("openid", openid);
+                          //result.set("gender",gender);  //再添加数据就不能正常初始化了
+                          result.save();
+                          //为用户添加空的项目“我的项目”
+                          //that.buildProject('我的项目','空项目')
+                          //为用户添加实例沟通模板
+                          //that.addCommunicateModel(user.id,1,communicate_sample_model1)
+                          //跳转到项目主页
+                          wx.switchTab({
+                            url: '../Project/Project',
+                          })
+                        }
+                      })
+              
+                      // wx.navigateBack({
+                      //   delta: 1
+                      // })
+                    }
+                  })
                 }
-              });
-              
-              
-              wx.navigateBack({
-                delta: 1
-              })
-            }
-          })
-        }
-      }
+              }
+            
+            })
+         })
+       }
     })
 
   },
-  
+  //创建项目
+  buildProject: function (title, desc) {
+    var that = this;
+    var Project = Bmob.Object.extend("project")
+    var project = new Project()
+    var currentUser = Bmob.User.current()
+    var leader_id = "0"
+    var leader_name = "0"
+
+    if (currentUser) {
+      leader_id = currentUser.id
+      leader_name = currentUser.get("nickName")
+      console.log("当前用户:", leader_id, leader_name)
+    }
+    console.log(title)
+    console.log(desc)
+    project.save({
+      name: title,
+      desc: desc,
+      leader_id: leader_id,
+      leader_name: leader_name,
+      is_first: false,
+      is_delete: false,
+      img_url: "http://bmob-cdn-19251.b0.upaiyun.com/2018/05/18/ff3371c040fe5b6380011eb3cb1770a4.png"  //涛哥找的默认图片
+    }, {
+        success: function (result) {
+          console.log("创建项目成功！", result)
+          that.addLeader(result.id, leader_id)  //当用户创建项目时，添加项目成员表，并指定为领导人
+          that.createTaskList(result.id/*项目id*/, "未完成"/*默认的任务列表名称*/)  //为用户创建默认的任务列表“未完成”
+          wx.hideLoading()
+          wx.showToast({
+            title: '成功创建项目',
+            icon: 'success',
+            duration: 1000
+          });
+          wx.switchTab({
+            url: '../Project',
+          })
+        },
+        error: function (result, error) {
+          wx.showToast({
+            title: '失败',
+            icon: 'none',
+          });
+          console.log("创建项目失败！", error)
+          //失败情况
+
+        }
+      })
+
+  },
+  /**
+* 2018-05-19
+* @author mr.li
+* @parameter projId 项目id，title任务看板名称
+* 创建任务看板
+*/
+  createTaskList: function (projId, title) {
+    var that = this
+    var TaskList = Bmob.Object.extend("task_list")
+    var taskList = new TaskList()
+
+    //添加任务看板
+    taskList.save({
+      title: title,
+      proj_id: projId,
+      is_delete: false
+    }, {
+        success: function (result) {
+          //添加任务看板成功
+
+        },
+        error: function (result, error) {
+          //添加任务看板失败
+          console.log("添加任务看板失败!")
+        }
+      })
+  },
+  /**
+ * @author mr.li
+ * @parameter projId项目id，userId用户id
+ * 当用户创建项目时，添加项目成员表，并指定为领导人
+ */
+  addLeader: function (projId, userId) {
+    var ProjectMember = Bmob.Object.extend("proj_member")
+    var projMember = new ProjectMember();
+    var project = Bmob.Object.createWithoutData('project', projId)
+
+    projMember.save({
+      proj_id: projId,
+      user_id: userId,
+      is_leader: true
+    }, {
+        success: function (result) {
+          //添加成功
+          console.log("保存项目领导成功！")
+        },
+        error: function (result, error) {
+          //添加失败
+          console.log("保存项目领导失败！", error)
+        }
+      })
+  },
+  /**
+  * @parameter userId用户id， _type模板类型 1 代表 ‘意见’，2 代表’提问‘， 3 代表’点赞‘, modelContent 模板内容
+  * 根据用户id 和 模板类型添加模板
+  */
+  addCommunicateModel: function (userId, _type, modelContent) {
+    var that = this
+    var CommunicateMod = Bmob.Object.extend('communicate_mod')
+    var communicatemod = new CommunicateMod()
+
+    //添加指定类型的沟通模板
+    if (modelContent != null && modelContent != '') {
+      communicatemod.save({
+        user_id: userId,  //用户id
+        type: _type,      //模板类型   1 代表 ‘意见’，2 代表’提问‘， 3 代表’点赞‘
+        content: modelContent  //模板内容
+      }, {
+          success: function (result) {
+            //成功
+            console.log('提示用户添加沟通模板成功过！')
+            wx.showToast({
+              title: '添加模板成功',
+            })
+            wx.navigateBack()
+
+
+          },
+          error: function (result, error) {
+            //失败
+            console.log('添加指定类型的沟通模板失败:', error)
+          }
+        })
+    }
+
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -79,11 +229,11 @@ Page({
    */
   onShow: function () {
 
-    // 取当前用户id
-    var currentUserId = Bmob.User.current().id
-    this.setData({
-      currentUserId: currentUserId
-    })
+    // // 取当前用户id
+    // var currentUserId = Bmob.User.current().id
+    // this.setData({
+    //   currentUserId: currentUserId
+    // })
     
   },
 
